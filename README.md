4287e6214adba9742268b8cacf4de23a349b000c
Mariusz Najwer <najwer23@gmail.com>
2021-11-30 21:45:19 +0100

    Init

--- README.md
+++ README.md
@@ -1,2 +1,43 @@
-# website-starter-pack-react
-website-starter-pack-react with webpack
+# Website Starter Pack - React
+
+## Inside
+- Babel, 
+- CSS, 
+- JavaScript
+- FontAwesome 5
+- Font Mulish
+- Webpack 
+    - MiniCssExtractPlugin
+    - HtmlWebpackPlugin
+    - CssMinimizerPlugin
+    - CleanWebpackPlugin
+    - File manager
+- HTML template
+
+## Usage
+### Installation
+```sh
+$ yarn install
+```
+
+### Dev
+```sh
+$ yarn start
+```
+
+### Production
+```sh
+$ yarn build
+```
+
+### Deploy
+```sh
+$ yarn deploy
+```
+
+### Author
+Mariusz Najwer
+[https://najwer23.github.io/](https://najwer23.github.io/)
+
+### License
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
---
+++ webpack.config.js
@@ -0,0 +1,77 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+
+var entryCustom = [
+  "index"
+].reduce((o, key) => ({ ...o, [key]: path.resolve(__dirname, "./src/" + key + ".js") }), {})
+  
+module.exports = {
+  entry: entryCustom,
+  output: {
+    filename: "[name].[fullhash].bunde.js",
+    path: path.resolve(__dirname, "./build"),
+  },
+  optimization: {
+    minimizer: ["...", new CssMinimizerPlugin()],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "./src/index.html"),
+      favicon: "./src/media/f.png",
+      minify: {
+        removeAttributeQuotes: true,
+        collapseWhitespace: true,
+        removeComments: true,
+      },
+      inject: "body",
+      // scriptLoading: "defer"
+    }),
+    new MiniCssExtractPlugin({
+      linkType: "text/css",
+      filename: "[name].[fullhash].css",
+      chunkFilename: "[id].css",
+    }),
+    new CleanWebpackPlugin(),
+  ],
+  target: ["web", "es5"],
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: [
+              [
+                "@babel/preset-env",
+                {
+                  targets: {
+                    ie: "11",
+                  },
+                },
+              ],
+              [
+                "@babel/preset-react",
+                {
+                  runtime: "automatic",
+                },
+              ],
+            ],
+          },
+        },
+      },
+      {
+        test: /\.(sa|sc|c)ss$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
+      },
+      {
+        test: /\.(jpe?g|svg|png|gif|ico|eot|ttf|woff2?)(\?v=\d+\.\d+\.\d+)?$/i,
+        type: "asset/resource",
+      },
+    ],
+  },
+};
